69d7b6072384539a075ca381b695709f63713ca7
Ricardo Castro <ricardocastro@Ricardos-MacBook-Air.local>
2018-03-08 22:54:31 -0500

    creating a logger in C

--- Scripts/pgenerator.c
+++ Scripts/pgenerator.c
@@ -1,15 +1,28 @@
 /*
 LPM made in C
+
+To Do:
+Needs to add logger info to the current logger but for testing its going to write on
+another logger.
+
+need to come up with an algorithm to generate passwords.
 */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 // #include <argp.h> for defining arguments, it cant be found.
 
 //#define VERSION 
+//Define Time
+size_t time;
+struct tm *timestamp;
+
 
 // Find out how to create a logger in C
 // handling arguments in C
+
+void logger(int code, char* buffer);
 
 int main(int argc, char *argv[]){
     if(argc == 1){
@@ -20,4 +33,15 @@ int main(int argc, char *argv[]){
     }
 
     return 0;
+}
+
+//code to determine warning, error or info and the buffer is the string with the message.
+void logger(int code, char* buffer){
+    //Define code
+    FILE *logfile;
+    if(code == 0)   //Error
+    {
+        fopen("pgeneratorclog.log", "a+");
+        
+    }
 }
