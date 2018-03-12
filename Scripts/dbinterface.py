6421d0f8efeef00b1f84e082117ed50c6523a0c1
Ricardo Castro <ricardocastro@Ricardos-MacBook-Air.local>
2018-03-11 23:28:57 -0400

    Still need to figure out how to do the database.

--- Scripts/dbinterface.py
+++ Scripts/dbinterface.py
@@ -2,18 +2,8 @@
 Database Interface for Pgenerator
 '''
 
+from pymongo import MongoClient
 
-
-from mongoengine import *
-import datetime
-
-class Account(Account):
-    uid = UUIDField()
-    username = StringField(max_length=20, required=True)
-    password = StringField(max_length=20, required=True)
-
-def main():
-    test = Account(uid='12dedwe', username='tester', password='testerpassword')
-
-if __name__ == '__main__':
-    main()
+client = MongoClient()
+db = client.admin
+serverStatusResult = db.command("serverStatus")
