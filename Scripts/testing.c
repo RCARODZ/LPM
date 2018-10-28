45722febc974dc116f3dc25b62b711aa909d5e32
Ricardo Castro <ricardocastro@Ricardos-MacBook-Air.local>
2018-10-28 00:09:11 -0400

    last changes

--- Scripts/testing.c
+++ Scripts/testing.c
@@ -1,7 +1,15 @@
 #include <sys/socket.h>
 #include <stdio.h>
 
-int main(){
+struct somestruct{
+    char next;
+    int test:4;
+    int test2:12;
+} __packeds;
+
+int main(int argc, char *argv[]){
     printf("working...\n");
+    struct somestruct t;
+    printf("%d\n", t.test2);
     return 0;
 }
